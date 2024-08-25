b52cd47da198b6137b3420b088dd0f937464fbdb
Nick Chan <towinchenmi@gmail.com>
2024-08-25 15:42:01 +0800

    proxyclient: pmu: skip over some unsupported parts

--- proxyclient/m1n1/hw/pmu.py
+++ proxyclient/m1n1/hw/pmu.py
@@ -10,19 +10,20 @@ class PMU:
 
     def __init__(self, u, adt_path=None):
         self.u = u
-        if adt_path is None:
-            adt_path = PMU.find_primary_pmu(u.adt)
+        #if adt_path is None:
+        #    adt_path = PMU.find_primary_pmu(u.adt)
 
-        self.node = u.adt[adt_path]
-        self.spmi = SPMI(u, adt_path.rpartition('/')[0])
-        self.adt_path = adt_path
-        self.primary = u.adt[adt_path].is_primary == 1
-        self.reg = u.adt[adt_path].reg[0]
+        #self.node = u.adt[adt_path]
+        #self.spmi = SPMI(u, adt_path.rpartition('/')[0])
+        #self.adt_path = adt_path
+        #self.primary = u.adt[adt_path].is_primary == 1
+        #self.reg = u.adt[adt_path].reg[0]
 
     def reset_panic_counter(self):
-        if self.primary:
-            leg_scrpad = self.node.info_leg__scrpad[0]
-            self.spmi.write8(self.reg, leg_scrpad + 2, 0) # error counts
+        #if self.primary:
+        #    leg_scrpad = self.node.info_leg__scrpad[0]
+        #    self.spmi.write8(self.reg, leg_scrpad + 2, 0) # error counts
+        return
 
     @staticmethod
     def find_primary_pmu(adt):
